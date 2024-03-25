570c4daccea7cd63637eaa3a0de6fb7214e1c672
cumaRull <rulsmods@gmail.com>
2024-03-25 23:08:40 +0700

    TWRP RMX3195 RUI2

---
+++ device.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := device/realme/RMX3195
+
+# Dynamic partition stuff
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Fastboot packages
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+	android.hardware.boot@1.2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.2.so
+	
+TARGET_RECOVERY_DEVICE_MODULES += \
+	vendor.display.config@1.0 \
+	vendor.display.config@2.0 \
+	libdisplayconfig.qti 
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)/libdisplayconfig.qti.so
